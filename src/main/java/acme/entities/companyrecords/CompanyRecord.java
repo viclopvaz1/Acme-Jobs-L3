522fa1331b12cd8fd78b0ccda71a1ae8349f3c73
dsanjose <dsanjose@rSanjoSEO-Lenovo-B50-10>
2019-11-19 23:56:40 +0100

    Restricciones de oro>plata>bronze y NotNull de CR-stars quitado.

--- src/main/java/acme/entities/companyrecords/CompanyRecord.java
+++ src/main/java/acme/entities/companyrecords/CompanyRecord.java
@@ -6,7 +6,6 @@ import javax.persistence.Index;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Range;
@@ -52,7 +51,6 @@ public class CompanyRecord extends DomainEntity {
 
 	private boolean				indication;
 
-	@NotNull
 	@Range(min = 0, max = 5)
 	private Integer				star;
 }
